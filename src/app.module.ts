4cec971dee8fa163528cbcd10701f65eecb248f9
nltruongvi <nltruongvi@gmail.com>
2021-09-01 14:24:19 +0700

    Update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+// Modules
 import { TodoFinalModule } from './example/todo-final/todo-final.module';
+
+//Controller
 import { TodoController } from './example/todo-controller/todo.controller';
 
 @Module({
-  imports: [TodoFinalModule],
+  imports: [TodoFinalModule, ConfigModule.forRoot()],
   controllers: [AppController, TodoController],
   providers: [AppService],
 })
